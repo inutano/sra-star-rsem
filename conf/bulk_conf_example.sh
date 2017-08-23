aded8bcab625ac6d89ccd1b9c2c7500dc41a9dd1
Tazro Inutano Ohta <inutano@gmail.com>
2017-08-23 16:21:12 +0900

    fixed path to external shell script to absolute path

--- conf/bulk_conf_example.sh
+++ conf/bulk_conf_example.sh
@@ -21,10 +21,10 @@ NUMBER_OF_PARALLEL_FTP=8
 # Set number of threads to be used by calculation
 NUMBER_OF_THREADS=2
 
-# Set path to the calculation workflow script
-WF_SCRIPT="./rnaseq_readcount.sh"
+# Set path to the calculation workflow script, should be absolute path
+WF_SCRIPT="${HOME}/repos/rnaseq-readcount-workflow/bin/rnaseq_readcount.sh"
 
-# Specify list of experiment IDs, a csv string or a file path.
+# Specify list of experiment IDs by csv string or file path.
 # If a file path was specified, the programm expect the file contains one ID per line.
 #EXPERIMENT_ID_LIST=/path/to/experiment_id_list.txt
 EXPERIMENT_ID_LIST="SRX534534,SRX1069901,SRX1069907"
